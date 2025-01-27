96b48a4c0312507a8fcb169a46ce1bf57d4e7a28
nikhil shrivastav <nikhilshrivastav8595716060@gmail.com>
2025-01-27 16:12:21 +0530

    add code related to update feature

---
+++ src/updateTask.js
@@ -0,0 +1,25 @@
+import { showAllTask } from "./ui.js";
+
+function updateTask(id) {
+    let newInput = prompt("Update Your Task"); //null or ""->falsy value
+    let regex = /^\d+$/;
+    let test = regex.test(newInput);
+
+    //check that user enter only valid input not number
+    if (test) {
+        alert("Invalid Input")
+        return
+    }
+
+    //check that user input is not a null and empty string
+    if (newInput) {
+        let allTask = localStorage.getItem("todoTask");
+        let convertIntoReadableFormat = JSON.parse(allTask);
+        convertIntoReadableFormat[id] = newInput;
+        localStorage.setItem("todoTask",JSON.stringify(convertIntoReadableFormat))
+        showAllTask()
+    }
+
+}
+
+export { updateTask }
